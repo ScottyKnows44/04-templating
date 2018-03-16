2d857bd389a3d0f659c20fc38ea9454513f651c4
sjgeyer <sjgeyer@comcast.net>
2018-03-16 11:06:37 -0700

    fix all bugs and finish lab

--- scott-sarah/scripts/article.js
+++ scott-sarah/scripts/article.js
@@ -13,22 +13,18 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-  let source = $('#article').html();
-
+  let source = $('#article-template').html();
+  console.log(source);
   let template = Handlebars.compile(source);
 
-
-
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  // DONE: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
-
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // REVIEW: The ternary operator above accomplishes this same logic.
+  // DONE: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
   //   this.publishStatus = `published ${this.daysAgo} days ago`;
   // } else {
@@ -39,8 +35,8 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// DONE: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+// Arrow functions only need parentheses around the parameters if there is more than one parameter.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
